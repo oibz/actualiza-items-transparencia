6cc5393b982de37d7caea4041b3607b2ac7bf308
oibz <curarrehueinfo@gmail.com>
2024-09-03 13:50:44 -0400

    Actualiza comentarios

--- actualizar_items_transparencia.py
+++ actualizar_items_transparencia.py
@@ -10,17 +10,17 @@ option = webdriver.ChromeOptions()
 driver = webdriver.Chrome(options=option)
 
 try:
-    # Abre el sitio web
+    # Abrir el sitio web
     driver.get("https://www.portaltransparencia.cl/PortalPdT/web/guest/home?p_p_id=58&p_p_lifecycle=0&p_p_state=maximized&p_p_mode=view&saveLastPath=0&_58_struts_action=%2Flogin%2Flogin")
 
-    # Inicia sesión
+    # Iniciar sesión
     username = driver.find_element(By.ID, "_58_login")
     password = driver.find_element(By.ID, "_58_password")
     username.send_keys("Usuario")
     password.send_keys("Contraseña")
     driver.find_element(By.NAME, "_58_fm").submit()
 
-    # Espera a que cargue la página de los items
+    # Esperar a que cargue la página de los items
     WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, "A2248:panel-vistas:infoTable_data")))
 
     while True:
@@ -37,15 +37,15 @@ try:
                         editar_button = item.find_element(By.CSS_SELECTOR, "td:nth-child(8) a")
                         editar_button.click()
 
-                        # Espera que cargue la nueva página y presiona "Nada a informar"
+                        # Selecciona la opción "Nada a informar"
                         WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, "A2248:panel-vistas:tabla-elementos:0:j_idt10441")))
                         driver.find_element(By.ID, "A2248:panel-vistas:tabla-elementos:0:j_idt10441").click()
 
-                        # Presiona el botón de "Aceptar"
+                        # Valida la actualización
                         WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, "A2248:panel-vistas:tabla-elementos:1:j_idt10445")))
                         driver.find_element(By.ID, "A2248:panel-vistas:tabla-elementos:1:j_idt10445").click()
 
-                        # Espera y desplázate al elemento "Volver al escritorio"
+                        # Se vuelve al Escritorio
                         element = WebDriverWait(driver, 20).until(
                             EC.presence_of_element_located((By.XPATH, '//a[@title="Volver al escritorio"]'))
                         )
@@ -68,12 +68,12 @@ try:
                     break  # Rompe el ciclo para re-evaluar la lista de elementos
 
             else:
-                # Si se completa el ciclo sin encontrar más elementos "Desactualizada", salir del while
+                # Si se completa el ciclo sin encontrar más elementos "Desactualizada", sale del while
                 break
 
         except StaleElementReferenceException:
             print("La página se ha actualizado, reintentando...")
-            continue  # Reintenta si la referencia al elemento es obsoleta
+            continue  # Reintenta si no pilla la referencia
 
 finally:
     # Cierra el navegador
